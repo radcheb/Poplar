f22e7cb34c3ef5ecf9940a4f2e795ee8daa1d27f
Kahla Wathek <wathek.kahla@gmail.com>
2018-06-19 20:15:00 +0200

    MainPage : translate language chine to english

--- poplar-app/src/MainPage.js
+++ poplar-app/src/MainPage.js
@@ -45,13 +45,13 @@ export default class MainPage extends Component {
       <View style={styles.container}>
         <NavigationBar
         style={{borderBottomWidth: 0.5, borderBottomColor: '#F3F3F3'}}
-        title={{title: '首页'}}/>
+        title={{title: 'Home'}}/>
         {this.state.barVisible && (!this.state.sent ?
         <View style={{height: 25,justifyContent: 'center',alignItems: 'center',backgroundColor: 'rgba(251, 189, 8, 0.8)'}}>
-          <Text style={{color: 'white'}}>发送中...</Text>
+          <Text style={{color: 'white'}}>sending...</Text>
         </View> :
         <View style={{height: 25,justifyContent: 'center',alignItems: 'center',backgroundColor: '#00B5AD',}}>
-          <Text style={{color: 'white'}}>已发送</Text>
+          <Text style={{color: 'white'}}>Has been sent</Text>
         </View>
         )
         }
